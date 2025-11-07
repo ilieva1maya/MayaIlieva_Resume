24d9d4e21bbe786a5edccfb68dced8935558391d
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-07 18:00:38 +0200

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
+import Footer from './components/Footer'; 
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Welcome/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
