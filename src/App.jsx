d05fec47f6a459382ed47b74150e5bce66320b06
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-08 14:47:25 +0200

    NavBar basic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Welcome from './components/Welcome';
 import Footer from './components/Footer'; 
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Navbar/>
       <Welcome/>
       <Footer/>
     </>
