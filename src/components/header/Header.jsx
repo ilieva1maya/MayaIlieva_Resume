5e1a97bfc0d19a150e956fc24d7bea68867dfe42
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-17 18:01:17 +0200

    Photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 export default function Header() {
     return (
         <div className="header-container">
-            <img src="/images/MayaIlieva_photo.png" alt="Maya Ilieva's photo" />
+            <img src="images/MayaIlieva_photo.png" alt="Maya Ilieva's photo" />
             <h1>Maya Ilieva</h1>
         </div>
     )
